d6cd9b862bf642912720060b6c4daa13c52b677e
John McCormick <jmccormick@carrierx.com>
2026-03-29 18:25:10 -0700

    Initial Separated from Python Added storage of last DRs Added Messaging

---
+++ README.md
@@ -0,0 +1,126 @@
+# Carrier CDR Download
+
+Download CarrierX Call Data Records via the api.
+
+The application requires NodeJS to run.
+
+## Installation
+
+In the terminal, change directory to the project root and run:
+
+```shell
+cd nodejs
+npm install
+```
+
+## Running
+
+In the `nodejs` directory, run the following
+
+```shell
+node cxGetCdrs.js <range | begin> <options> outputFileName.csv
+```
+
+## Commands - latest | range
+
+There are two commands available: `latest` & `range`. `latest` gets records since the last time it was run. `range` gets records in some date range.
+
+### Command latest
+
+The `latest` command tells the downloader to get the records since the last time it was called. This mode is intended to be run on a regular basis (e.g. daily), so it keeps track of the last record received and picks up where it left off.  By default, it will use `./data.json` to store SIDs, but you can specify the file to use.
+
+| Short | Long | Required | Description |
+| ----- | ---- | -------- | ----------- |
+| -t | --token | Required | Security access token available via the API or in the portal. [Documentation](https://www.carrierx.com/documentation/quick-start/token?show_admin=true#introduction) |
+| -d | --data-file | Optional | The file where the app should store the SIDs (record identifiers) for the last record downloaded. **Default - `./data.json`** |
+| -b *&lt;date&gt;* | --begin *&lt;date&gt;* | Required | The begining date (and optionally time), inclusive, in ISO 8601 format - YYYY-MM-DD[T[HH:[MM:[SS][Z]]]]. Dates and times are UTC. |
+| -e *&lt;date&gt;* | --end *&lt;date&gt;* | Optional | The end date (and optionally time), exclusive, in ISO 8601 format - YYYY-MM-DD[THH:MM:SS]. Dates and times are UTC. **Default - now**. |
+| -f  *&lt;format&gt;*| --format *&lt;format&gt;* | Optional | The output format, `csv` or `json`. **Default - `csv`** |
+| -o | --overwrite | Optional | Overwrite the output file. By default the app will not overwrite the output file. |
+| -y | --type | Optional | The type of records to get. Choices: `call` | `conference` | `message`. **Default - `call`** |
+| -h | --help | Optional | Display command help |
+
+#### File name - Required
+
+You most specify an output file name where records will be written. The app will create the output file but it will **not** create any directories.
+
+### Command range
+
+| Short | Long | Required | Description |
+| ----- | ---- | -------- | ----------- |
+| -t | --token | Required | Security access token available via the API or in the portal. [Documentation](https://www.carrierx.com/documentation/quick-start/token?show_admin=true#introduction) |
+| -b *&lt;date&gt;* | --begin *&lt;date&gt;* | Required | The begining date (and optionally time), inclusive, in ISO 8601 format - YYYY-MM-DD[THH:MM:SS]. Dates and times are UTC. |
+| -e *&lt;date&gt;* | --end *&lt;date&gt;* | Optional | The end date (and optionally time), exclusive, in ISO 8601 format - YYYY-MM-DD[THH:MM:SS]. Dates and times are UTC. **Default - now**. |
+| -f  *&lt;format&gt;*| --format *&lt;format&gt;* | Optional | The output format, `csv` or `json`. **Default - `csv`** |
+| -o | --overwrite | Optional | Overwrite the output file. By default the app will not overwrite the output file. |
+| -y | --type | Optional | The type of records to get. Choices: `call` | `conference` | `message`. **Default - `call`** |
+| -h | --help | Optional | Display command help |
+
+## Examples
+
+__NOTE__ Use caution entering an access token on the command line as commands, and therefore tokens, can be saved in history files.  All examples below use an environment variable `$CX_TOKEN` where the token is stored.
+
+### Get all CDRs
+
+```shell
+python3 cxGetCdrs.js latest --token $CX_TOKEN path_to/cdrs.csv
+```
+
+__CAUTION__ The first time this is run, it has the potential to create a very large file.
+
+### Since some date
+
+```shell
+node cxGetCdrs.js latest --token $CX_TOKEN --begin 2024-10-31 path_to/cdrs.csv
+```
+or
+
+```shell
+node cxGetCdrs.js range --token $CX_TOKEN --begin 2024-10-31 path_to/cdrs.csv
+```
+
+The only difference is that using command `range`, the state won't be save - the last record SID isn't stored.
+
+### Date range
+
+```shell
+node cxGetCdrs.js range --token $CX_TOKEN --begin 2024-10-31 --end 2024-11-06 path_to/cdrs.csv
+```
+
+The `end` is exclusive, so only records before `end` are included.
+
+### Time range - 1 hour
+
+```shell
+node cxGetCdrs.js --token $CX_TOKEN --begin 2024-10-31T18:00:00 --end 2024-10-31T19:00:00 path_to/cdrs.csv
+```
+
+### As JSON
+
+```shell
+node cxGetCdrs.js latest --token $CX_TOKEN --format json path_to/cdrs.json
+```
+
+### Overwrite output
+
+By default, the app will not overwrite an existing file. If you want to overwrite the file, you need to specify `-o` or `--overwrite`.
+
+```shell
+node cxGetCdrs.js --token $CX_TOKEN --begin 2024-10-31 --overwrite path_to/cdrs.csv
+```
+
+### Conference records
+
+If you want conference records instead of SIP call records, specify `-y conference` or `--type conference`.
+
+```shell
+node cxGetCdrs.js --token --type conference path_to/cdrs.csv
+```
+
+### Message Delivery records
+
+If you want message delivery records instead of SIP call records, specify `-y message` or `--type message`.
+
+```shell
+node cxGetCdrs.js --token --type conference path_to/cdrs.csv
+```
---
+++ cxGetCdrs.js
@@ -0,0 +1,321 @@
+#!/usr/bin/env nodejs
+
+const {Argument, Command, program, Option} = require('commander');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
+const jsonfile = require('jsonfile');
+const path = require('path');
+
+
+const BASE_URL = 'https://api.carrierx.com/core/v2';
+const CDR_URL_CALLS = `${BASE_URL}/calls/call_drs`;
+const CDR_URL_CONFERENCE = `${BASE_URL}/app/conference/calls`;
+const CDR_URL_MESSAGE = `${BASE_URL}/sms/message_drs`;
+const PAGE_SIZE = 1_000
+const DEFAULT_STATE_DATA_FILE = './state.json'
+
+
+class CdrGetter {
+  #begin;
+  #csvWriter;
+  #command;
+  #stateData;
+  #stateDataFile;
+  #end;
+  #filename;
+  #format;
+  #jsonData;
+  #nextUrl;
+  #overwrite;
+  #type;
+  #token;
+  #url;
+
+  constructor(command, filename, options) {
+    this.#stateData = {
+      call_sid: null,
+      conference_sid: null,
+      message_sid: null,
+    };
+    this.#jsonData = new Array();
+    this.#token = options.token;
+    this.#command = command;
+    this.#type = options.type;
+    try {
+      this.#begin = this.#getDateString(options.begin);
+    } catch (error) {
+      if (this.#command === 'range') {
+        throw new Error('Invalid start date');
+      } if (options.begin) {
+        throw new Error('Invalid begin date');
+      }
+    }
+    try {
+      this.#end = this.#getDateString(options.end);
+    } catch (error) {
+      this.#end = this.#getDateString(new Date().toISOString());
+    }
+    this.#format = options.format;
+    this.#nextUrl = null;
+    this.#overwrite = options.overwrite;
+    this.#stateDataFile = options.dataFile;
+    switch (this.#type) {
+      case 'conference':
+        this.#url = CDR_URL_CONFERENCE;
+        break;
+      case 'message':
+        this.#url = CDR_URL_MESSAGE;
+        break;
+      case 'call':
+        this.#url = CDR_URL_CALLS;
+        break;
+    }
+    this.#filename = filename;
+    if (!this.#dirExists(this.#filename)) {
+      throw new Error(`Directory "${path.dirname(this.#filename)}" does not exist.`);
+    }
+    if (fs.existsSync(this.#filename) && !this.#overwrite) {
+      throw new Error(`Output file exists. Use --overwrite to overwrite output file.`);
+    }
+  }
+
+  #dirExists = (filename) => {
+    try {
+      return fs.existsSync(path.dirname(filename));
+    } catch (error) {
+      return false;
+    }
+  }
+
+  #getDateString = (date) => {
+      return this.#getCleanDate(date).toISOString().split('.')[0] + 'Z';
+  }
+
+  #getCleanDate = (date) => {
+    return this.#hasTimeZone(date) ? new Date(date) : new Date(`${date}Z`);
+  }
+
+  #hasTimeZone = (date) => {
+    return Boolean(date.match(/[+-][\d][\d]:[\d][\d]$/) || date.match(/Z$/));
+  }
+
+  #loadStateData = async () => {
+    if (this.#command === 'latest') {
+      try {
+        this.#stateData = await jsonfile.readFile(this.#stateDataFile);
+      } catch (error) {
+        this.#stateData = {
+          call_sid: null,
+          conference_sid: null,
+          message_sid: null,
+        };
+      }
+    }
+  }
+
+  #saveStateData = async () => {
+    if (this.#command === 'latest') {
+      await jsonfile.writeFile(
+        this.#stateDataFile,
+        this.#stateData,
+        {spaces: 2}
+      );
+    }
+  }
+
+  #getLastSid = () => {
+    if (this.#type === 'call') {
+      return this.#stateData.call_sid;
+    } else if (this.#type === 'conference') {
+      return this.#stateData.conference_sid;
+    } else if (this.#type === 'message') {
+      return this.#stateData.message_sid;
+    }
+  }
+
+  #setLastSid = (item) => {
+    if (this.#type === 'call') {
+      this.#stateData.call_sid = item?.dr_sid;
+    } else if (this.#type === 'conference') {
+      this.#stateData.conference_sid = item?.call_sid;
+    } else if (this.#type === 'message') {
+      this.#stateData.message_sid = item?.dr_sid;
+    }
+    this.#saveStateData();
+  }
+
+  #isBeyondEnd = (cdr) => {
+    if (this.#command === 'range') {
+      return this.#getCleanDate(cdr.date_stop) >= this.#getCleanDate(this.#end);
+    }
+    return false;
+  }
+
+  #filterItems = (items) => {
+    return items.filter(item => !this.#isBeyondEnd(item));
+  }
+
+  #fetchCdrs = async (url, params) => {
+    const u = new URL(url);
+    for (const [key, value] of Object.entries(params || {})) {
+      if (value != null) {
+        u.searchParams.set(key, String(value));
+      }
+    }
+    const r = await fetch(u.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.#token}`,
+      },
+    });
+    const data = await r.json()
+    this.#nextUrl = data?.pagination?.next;
+    return data;
+  }
+
+  #getFirstCdr = async () => {
+    const params = {
+      limit: 1,
+      order: 'date_stop',
+    };
+    if (this.#begin) {
+      params.filter = `date_stop ge ${this.#getDateString(this.#begin)}`;
+    }
+    return await this.#fetchCdrs(this.#url, params);
+  }
+
+  #getCdrsLatest = async () => {
+    const params = {
+      order: 'date_stop',
+      after: this.#getLastSid(),
+    };
+    return await this.#fetchCdrs(this.#url, params);
+  }
+
+  #cdrRowForCsv = (cdr) => {
+    const row = {...cdr};
+    const ud = row.user_data;
+    if (ud != null && typeof ud === 'object') {
+      row.user_data = JSON.stringify(ud);
+    }
+    return row;
+  }
+
+  #initCsv = async (cdr) => {
+    const header = new Array();
+    for (const key in cdr) {
+      header.push({
+        id: key,
+        title: key,
+      });
+    }
+    this.#csvWriter = createCsvWriter({
+      path: this.#filename,
+      header,
+    });
+  }
+
+  #writeCsv = async (cdrs) => {
+    const rows = cdrs.items.map((item) => this.#cdrRowForCsv(item));
+    if (!this.#csvWriter) {
+      this.#initCsv(rows[0]);
+    }
+    await this.#csvWriter.writeRecords(rows);
+  }
+
+  get = async () => {
+    await this.#loadStateData();
+    let page = 0;
+    let willBreak = false;
+    while (true) {
+      let isSingleFetch = false;
+      console.log(`Page: ${page}`);
+      let cdrs = null;
+      if (page === 0) {
+        if (this.#begin) {
+          cdrs = await this.#getFirstCdr();
+          isSingleFetch = true;
+        } else {
+          cdrs = await this.#getCdrsLatest();
+        }
+      } else if (page === 1 && this.#begin) {
+        cdrs = await this.#getCdrsLatest();
+      } else {
+        cdrs = await this.#fetchCdrs(this.#nextUrl, {});
+      }
+      if (cdrs?.count === 0) break;
+      if (this.#command === 'range' && this.#isBeyondEnd(cdrs.items[cdrs.items.length - 1])) {
+        cdrs.items = this.#filterItems(cdrs.items);
+        willBreak = true;
+      }
+      this.#setLastSid(cdrs?.items?.[cdrs?.items?.length - 1]);
+      if (this.#format === 'csv') {
+        await this.#writeCsv(cdrs);
+      } else {
+        this.#jsonData = this.#jsonData.concat(cdrs.items);
+      }
+      if (willBreak || (!isSingleFetch && cdrs.count < PAGE_SIZE)) break;
+      page++;
+    }
+    if (this.#format === 'json') {
+      await jsonfile.writeFile(
+        this.#filename,
+        this.#jsonData,
+        {spaces: 2}
+      );
+    }
+  }
+}
+
+const main = async (command, filename, options) => {
+  try {
+    const getter = new CdrGetter(command, filename, options);
+    getter.get();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+optionToken = new Option('-t, --token <access_token>', 'Security access token available in the portal.').makeOptionMandatory();
+optionBegin = new Option('-b, --begin <date>', 'The begining date (and optionally time), inclusive, in ISO 8601 format.');
+optionEnd = new Option('-e, --end <date>', 'The ending date (and optionally time), exclusive, in ISO 8601 format. Default is now.');
+optionFormat = new Option('-f, --format <format>', 'output format')
+  .choices(['csv', 'json'])
+  .default('csv');
+optionOverwrite = new Option('-o, --overwrite', 'Overwrite the output file. By default will not overwrite.');
+optionType = new Option('-y, --type <type>', 'The type of CDR to get. Default is call.')
+  .choices(['call', 'conference', 'message'])
+  .default('call');
+optionDataFile = new Option('-d, --data-file <data_file>', 'The file in which to store the last SID for the next run. Default is ' + DEFAULT_STATE_DATA_FILE)
+  .default(DEFAULT_STATE_DATA_FILE);
+argFilename = new Argument('<filename>', 'File where the CDRs should be written');
+
+const latestCommand = new Command('latest')
+  .description('Download CarrierX call detail records after the last retrieved CDR')  
+  .addOption(optionToken)
+  .addOption(optionBegin)
+  .addOption(optionFormat)
+  .addOption(optionOverwrite)
+  .addOption(optionType)
+  .addOption(optionDataFile)
+  .addArgument(argFilename)
+  .action((filename, options) => main('latest', filename, options));
+
+const rangeCommand = new Command('range')
+  .description('Download CarrierX call detail records between two dates')
+  .addOption(optionToken)
+  .addOption(optionBegin)
+  .addOption(optionEnd)
+  .addOption(optionFormat)
+  .addOption(optionOverwrite)
+  .addOption(optionType)
+  .addArgument(argFilename)
+  .action((filename, options) => main('range', filename, options));
+
+program
+  .description('Download CarrierX call detail records')
+  .addCommand(latestCommand)
+  .addCommand(rangeCommand);
+  program.parse()
+  const options = program.opts();
